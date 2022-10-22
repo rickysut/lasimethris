d5c4daf71fa8512a8675102cd18335b98fb3c1ae
Ricky Sutanto <ricky@sali-evo.com>
2022-10-22 19:26:34 +0700

    template 2

--- resources/views/admin/dashboard/indexadmin.blade.php
+++ resources/views/admin/dashboard/indexadmin.blade.php
@@ -423,7 +423,70 @@
 			[0, 'asc']
 		],
         orderCellsTop: true,
-    	
+    	dom:
+			/*	--- Layout Structure 
+				--- Options
+				l	-	length changing input control
+				f	-	filtering input
+				t	-	The table!
+				i	-	Table information summary
+				p	-	pagination control
+				r	-	processing display element
+				B	-	buttons
+				R	-	ColReorder
+				S	-	Select
+
+				--- Markup
+				< and >				- div element
+				<"class" and >		- div with a class
+				<"#id" and >		- div with an ID
+				<"#id.class" and >	- div with an ID and a class
+
+				--- Further reading
+				https://datatables.net/reference/option/dom
+				--------------------------------------
+			 */
+			"<'row mb-3'<'col-sm-12 col-md-6 d-flex align-items-center justify-content-start'f><'col-sm-12 col-md-6 d-flex align-items-center justify-content-end'lB>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+		buttons: [
+			{
+				extend:    'colvis',
+				text:      'Visibility',
+				titleAttr: 'Col visibility',
+				className: 'btn-outline-info btn-xs mr-sm-3 ml-5'
+			},
+			{
+				extend: 'pdfHtml5',
+				text: 'PDF',
+				titleAttr: 'Generate PDF',
+				className: 'btn-outline-danger btn-xs mr-1'
+			},
+			{
+				extend: 'excelHtml5',
+				text: 'Excel',
+				titleAttr: 'Generate Excel',
+				className: 'btn-outline-success btn-xs mr-1'
+			},
+			/*{
+				extend: 'csvHtml5',
+				text: 'CSV',
+				titleAttr: 'Generate CSV',
+				className: 'btn-outline-primary btn-xs mr-1'
+			},*/
+			/*{
+				extend: 'copyHtml5',
+				text: 'Copy',
+				titleAttr: 'Copy to clipboard',
+				className: 'btn-outline-primary btn-xs mr-1'
+			},*/
+			{
+				extend: 'print',
+				text: 'Print',
+				titleAttr: 'Print Table',
+				className: 'btn-outline-primary btn-xs'
+			}
+		]
     	
     	//
         
